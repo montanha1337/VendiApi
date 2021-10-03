3431f6e1aceb9b7691e871226654925b9bbee3a1
Matheus Fernandes <matheusfernandes3m@hotmail.com>
2021-10-03 20:08:15 -0300

    feat: add image upload

--- src/controller/Anunciocontroller.js
+++ src/controller/Anunciocontroller.js
@@ -3,67 +3,78 @@ import Funcao from './functions'
 import Consulta from '../Banco/migrations/consulta'
 import Cadastro from '../Banco/migrations/insert'
 import multer from 'multer'
+const fs = require('fs');
+const path = require("path");
 import { Token } from 'sucrase/dist/parser/tokenizer'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, `${__dirname}/uploads/anuncio`)
+        fs.mkdir('./uploads/anuncio', (err) => {
+            cb(null, './uploads/anuncio');
+        });
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.originalname)
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, uniqueSuffix + file.originalname)
     }
-  })
+})
+
+
 const parser = multer({ storage: storage })
 const router = express.Router()
-router.post('/inserir',parser.single('imagem'), async (req, res,next ) => {
-    
-    var anuncio       = Object()
-    anuncio.file      = req.file
+
+router.get('/uploads/:id', async (req, res) => {
+    let filepath = path.join(__dirname + `/../../uploads/anuncio/${req.params.id}`);
+    res.sendFile(filepath);
+});
+router.post('/inserir', parser.single('imagem'), async (req, res, next) => {
+
+    var anuncio = Object()
+    anuncio.file = req.file
     anuncio.categoria = req.body.categoria
-    anuncio.titulo    = req.body.titulo
+    anuncio.titulo = req.body.titulo
     anuncio.descricao = req.body.descricao
-    anuncio.valor     = req.body.valorVenda
-    anuncio.data      = new Date();
-   const token1 = req.headers.authorization.replace(/^Bearer\s/, '');
-    const token = Funcao.atualizajwt(token1) 
-    if(token.status == false){
+    anuncio.valor = req.body.valorVenda
+    anuncio.data = new Date();
+    const token1 = req.headers.authorization.replace(/^Bearer\s/, '');
+    const token = Funcao.atualizajwt(token1)
+    if (token.status == false) {
         console.log(anuncio.mensagem)
-        res.status(401).json({token:null})
-    }else{
-        const idAnuncio = await Cadastro.anuncio(token,anuncio)
-       if(idAnuncio.status == false){
+        res.status(401).json({ token: null })
+    } else {
+        const idAnuncio = await Cadastro.anuncio(token, anuncio)
+        if (idAnuncio.status == false) {
             console.log(idAnuncio.mensagem)
-           res.status(200).json({token:null,idAnuncio:null})
-        }else{
-            res.status(200).json({token,idAnuncio})
+            res.status(200).json({ token: null, idAnuncio: null })
+        } else {
+            res.status(200).json({ token, idAnuncio })
 
         }
     }
 })
-router.get('/buscar/:id', async (req, res, ) => {
+router.get('/buscar/:id', async (req, res,) => {
 
     const { id } = req.params
     const anuncio = await Consulta.anuncioLista(id)
-    if(anuncio.status == false){
+    if (anuncio.status == false) {
         console.log(anuncio.mensagem)
-        res.status(200).json({anuncio:[]})
-    }else{
-        res.status(200).json({anuncio})
+        res.status(200).json({ anuncio: [] })
+    } else {
+        res.status(200).json({ anuncio })
     }
-           
+
 })
-router.get('/deletar/:id', async (req, res, ) => {
+router.get('/deletar/:id', async (req, res,) => {
     const tokenVenda = req.headers.authorization.replace(/^Bearer\s/, '')
     const { id } = req.params
-    const anuncio = await Cadastro.deletaAnuncio(id,tokenVenda)
-    if(anuncio.status == false){
+    const anuncio = await Cadastro.deletaAnuncio(id, tokenVenda)
+    if (anuncio.status == false) {
         console.log(anuncio.mensagem)
-        res.status(200).json({anuncio:[]})
-    }else{
-        res.status(200).json({anuncio})
+        res.status(200).json({ anuncio: [] })
+    } else {
+        res.status(200).json({ anuncio })
     }
-           
+
 })
 
 
