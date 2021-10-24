7673195b391720b5585807079b080d9a060bde15
montanha1337 <matheusdteixeira@hotmail.com>
2021-10-24 13:05:02 -0300

    teste

--- src/controller/Desenvolvedor.js
+++ src/controller/Desenvolvedor.js
@@ -20,17 +20,17 @@ router.get('/testeConexaoBanco', async (req, res, ) => {
     const result = await Banco.session("SELECT count(nspname) FROM pg_catalog.pg_namespace;")
     
     console.log(result)
-   // if(result.count==7){
-   //   const texto= await Funcao.verificaconexao(1)
-   //   res.json(texto)
+    if(result.count==7){
+      const texto= await Funcao.verificaconexao(1)
+      res.json(texto)
  
-  //}
-   // else{
-  //  Funcao.atualizabanco()
-     // const conexao = await Funcao.verificaconexao(2)
-    //  res.json(conexao)
-    //}
-    res.json({result})
+  }
+    else{
+      Funcao.atualizabanco()
+      const conexao = await Funcao.verificaconexao(2)
+      res.json(conexao)
+    }
+    //res.json({result})
 })
  // rota de deletar de banco de dados.
  router.get('/deletaBanco',async(req,res)=>{
