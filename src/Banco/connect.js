57cc7239e0f9dce8b7009f567762d68dab1c9fc6
montanha1337 <matheusdteixeira@hotmail.com>
2021-10-24 12:45:14 -0300

    teste conexao

--- src/Banco/connect.js
+++ src/Banco/connect.js
@@ -3,7 +3,7 @@ import {Pool,Client} from 'pg'
 // criar conexão com a base de dados
 // deve-se ao instalar o banco de dados colocar #abc123# no password
 
-const client = new Pool({
+const client = new Client({
   connectionString: 'postgres://odltvcxcrvlktv:910a2e991a078df3456e3c0df561cef3691662c53c5b13283c13c330ad39c2de@ec2-52-3-79-87.compute-1.amazonaws.com:5432/d862rgr5db37ii',
   ssl: {
     rejectUnauthorized: false,
