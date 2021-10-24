2234ba2bf90597f988cb8cb44134fdb557e04c15
montanha1337 <matheusdteixeira@hotmail.com>
2021-10-24 11:02:28 -0300

    teste

--- src/Banco/connect.js
+++ src/Banco/connect.js
@@ -1,15 +1,18 @@
-import {Client} from 'pg'
+import {Pool,Client} from 'pg'
 
 // criar conexão com a base de dados
 // deve-se ao instalar o banco de dados colocar #abc123# no password
 
-const client = new Client({
+const client = new Pool({
   Host:'ec2-52-3-79-87.compute-1.amazonaws.com',
   Database:'d862rgr5db37ii',
   User:'odltvcxcrvlktv',
   Port:5432,
   Password:'910a2e991a078df3456e3c0df561cef3691662c53c5b13283c13c330ad39c2de',
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+    ssl:true
+  },
   connectionTimeoutMillis: 1500
 })
 
