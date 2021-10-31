799da7511488ecec43fe315f4301078c16477c30
montanha1337 <matheusdteixeira@hotmail.com>
2021-10-31 05:35:11 -0300

    conexao

--- src/web/WebController.js
+++ src/web/WebController.js
@@ -3,10 +3,10 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    res.sendFile(__dirname+'/controller/web/PaginaInicial.html')
+    res.sendFile(__dirname+'/PaginaInicial.html')
 });
 router.get('/redefinirSenha', async (req, res, next) => {
-    res.sendFile(__dirname+'/web/recuperarsenha.html')
+    res.sendFile(__dirname+'/recuperarsenha.html')
 })
 router.get('/buscar/', async (req, res,) => {
 
