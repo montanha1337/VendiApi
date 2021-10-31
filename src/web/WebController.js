54b433b94efd84d599decdb22247361337237e77
montanha1337 <matheusdteixeira@hotmail.com>
2021-10-31 06:09:16 -0300

    webpage

--- src/web/WebController.js
+++ src/web/WebController.js
@@ -10,10 +10,10 @@ router.get('/redefinirSenha/:token', async (req, res, next) => {
     res.sendFile(caminho+'/recuperarsenha.html')
 })
 router.get('/teste', async (req, res,) => {
-    res.sendFile(caminho+ '')
+    res.sendFile(caminho+ '/login.html')
 })
 router.get('/deletar/', async (req, res,) => {
-    res.sendFile(caminho+ '/login.html')
+    res.sendFile(caminho+ '')
 })
 router.get('/deletarfoto/', async (req, res,) => {
     res.sendFile(caminho+ '')
