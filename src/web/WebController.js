2b2bde9a594725c6ee4e8d661e9ec0a0c3e6cbc1
montanha1337 <matheusdteixeira@hotmail.com>
2021-10-31 05:50:19 -0300

    teste

--- src/web/WebController.js
+++ src/web/WebController.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get('/', async (req, res) => {
     res.sendFile(__dirname+'/PaginaInicial.html')
 });
-router.get('/redefinirSenha', async (req, res, next) => {
+router.get('/redefinirSenha/:token', async (req, res, next) => {
     res.sendFile(__dirname+'/recuperarsenha.html')
 })
 router.get('/buscar/', async (req, res,) => {
