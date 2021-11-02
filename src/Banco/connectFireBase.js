e62850ca4d9483779fb353e53cc72831d460cd98
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-02 04:02:48 -0300

    teste imagem

--- src/Banco/connectFireBase.js
+++ src/Banco/connectFireBase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase";
+import firebase from "firebase/app";
 
   // Set the configuration for your app
   // TODO: Replace with your app's config object
