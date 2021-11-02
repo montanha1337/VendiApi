5f18bbc45abf4a2d0da8df286d88b2521be28fd7
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-02 03:57:30 -0300

    teste imagem

--- src/Banco/connectFireBase.js
+++ src/Banco/connectFireBase.js
@@ -1,21 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/app";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAgbdpzrvvDgceKoAVH4naCDuzxPgaI1PM",
-  authDomain: "vendi-527e3.firebaseapp.com",
-  projectId: "vendi-527e3",
+  // Set the configuration for your app
+  // TODO: Replace with your app's config object
+  var firebaseConfig = {
+    apiKey: '"AIzaSyAgbdpzrvvDgceKoAVH4naCDuzxPgaI1PM"',
+    authDomain: 'vendi-527e3.firebaseapp.com',
+    projectId: "vendi-527e3",
   storageBucket: "vendi-527e3.appspot.com",
   messagingSenderId: "250439411600",
   appId: "1:250439411600:web:3ae7720f2e64aaeab47515"
-};
+  };
+  firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-var storage = app.storage()
-
+  // Get a reference to the storage service, which is used to create references in your storage bucket
+  var storage = firebase.storage();
 module.exports={firebaseConfig, storage}
