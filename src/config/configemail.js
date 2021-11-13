4f629255ee585ae84b88d2837d9f53fbf34a4d74
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-13 17:25:51 -0300

    teste

--- src/config/configemail.js
+++ src/config/configemail.js
@@ -26,30 +26,4 @@ async function email(email, nome, mensagem){
       // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com
     }
 
-
-var server = http.createServer();
-server.addListener('request', function(req, res) {
-  var chunks = [];
-  req.on('data', chunks.push.bind(chunks));
-  req.on('end', function() {
-    var mailparser = new MailParser();
-    mailparser.on("end", function(mail_object) {
-
-      // API for https://github.com/andris9/mailparser
-      mail_object.from; // [ { address: 'sender@example.com', name: 'Sender Name' } ]
-      mail_object.to;   // [ { address: 'example@mail2webhook.com', name: '' } ]
-      mail_object.subject; // "Testing 1 2 3"
-      mail_object.html;
-      mail_object.text;
-      console.log(mail_object.from, mail_object.to, mail_object.subject);
-      console.log(mail_object);
-
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.end();
-    });
-    var params = querystring.parse(chunks.join("").toString());
-    mailparser.write(params['message']);
-    mailparser.end();
-  });
-});
     module.exports={email}
