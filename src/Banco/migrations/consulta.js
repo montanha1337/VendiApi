2fd30e4fe7fbdf4af0c50da7573577a183ba874e
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-21 12:09:15 -0300

    correção insert cliente

--- src/Banco/migrations/consulta.js
+++ src/Banco/migrations/consulta.js
@@ -49,7 +49,7 @@ async function vendedor(token) {
       const erro = Funcoes.padraoErro("nao foi possivel identificar o usuario da requisição")
       return erro
     }else{
-      const cliente = await Banco.session(`select p.id_pessoa, u.nome, p.cpf, t.telefone, e.cidade, t.whatsapp, e.rua, e.bairro, e.cidade, e.numero, e.cep  from Vendi.user u left outer join Vendi.pessoa   p on p.id_user=   u.id_user left outer join Vendi.telefone t on t.id_pessoa= p.id_pessoa left outer join Vendi.endereco e on e.id_pessoa= p.id_pessoa left outer join Vendi.vendedor v on v.id_pessoa= p.id_pessoa where u.id_user = = ${user}`)
+      const cliente = await Banco.session(`select p.id_pessoa, u.nome, p.cpf, t.telefone, e.cidade, t.whatsapp, e.rua, e.bairro, e.cidade, e.numero, e.cep  from Vendi.user u left outer join Vendi.pessoa   p on p.id_user=   u.id_user left outer join Vendi.telefone t on t.id_pessoa= p.id_pessoa left outer join Vendi.endereco e on e.id_pessoa= p.id_pessoa left outer join Vendi.vendedor v on v.id_pessoa= p.id_pessoa where u.id_user = ${user}`)
       if(cliente.rows[0]){
         return cliente.rows[0]
       }
