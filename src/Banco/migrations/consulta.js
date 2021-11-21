1a8411b08132dab091400127624b3e2a03a65507
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-21 13:17:34 -0300

    teste

--- src/Banco/migrations/consulta.js
+++ src/Banco/migrations/consulta.js
@@ -211,7 +211,8 @@ async function fotoAnuncio(linkFoto) {
 async function anuncioNegocicao(idAnuncio) {
   var anuncio = await Banco.session(`select a.titulo, t.telefone from Vendi.anuncio a left outer join Vendi.vendedor v on v.id_vendedor = a.id_vendedor left outer join Vendi.telefone t on t.id_pessoa = v.id_pessoa where a.id_anuncio = ${idAnuncio} and t.whatsapp = 'true'`)
   if(anuncio.rows[0]){
-    var anuncios.descricao = anuncio.rows[0].titulo.replace(' ', '%20')
+    var anuncios = Object()
+    anuncios.descricao = anuncio.rows[0].titulo.replace(' ', '%20')
     anuncios.telefone = anuncio.rows[0].telefone
     console.log(anuncios)
     return anuncios
