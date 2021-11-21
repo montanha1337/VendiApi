cf5b63e4ad1fe19a3cf147a0bfcd0cdc2d001035
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-21 06:54:43 -0300

    teste

--- src/controller/Vendedorcontroller.js
+++ src/controller/Vendedorcontroller.js
@@ -35,6 +35,7 @@ router.post('/Inserir', async (req, res, ) => {
             res.status(401).json({token:null,tokenVenda:null,result:null})
         }else{
             result = await Cadastro.vendedor(token,data)
+            console.log(result)
             if(result.status== false){
                 console.log(result)
                 if(result.id_vendedor== null){
