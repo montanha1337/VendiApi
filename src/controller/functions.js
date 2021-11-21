308e5fc91c455fb9263a02de03f3d88ab69dee69
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-21 09:26:19 -0300

    teste calculo

--- src/controller/functions.js
+++ src/controller/functions.js
@@ -3,7 +3,7 @@ import Chave from '../Banco/connect'
 import Atualiza from '../Banco/migrations/database'
 import EnvioEmail from '../config/configemail'
 import bcrypt from 'bcrypt'
-import { unlink } from 'fs/promises';
+import Math   from 'core-js/fn/math'
 
 var salt = bcrypt.genSaltSync(10)
 
@@ -149,18 +149,35 @@ async function verificaconexao(mensagem){
         return true
     }
 
-    function distanciaLatLong(lat1, lon1, lat2, lon2) {
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat2-lat1);  // deg2rad below
-        var dLon = deg2rad(lon2-lon1); 
-        var a = 
-          Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-          Math.sin(dLon/2) * Math.sin(dLon/2)
-          ; 
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        var d = R * c; // Distance in km
-        return d;
+    function distanciaLatLong(lat1, long1, lat2, long2)
+    {    
+  
+        //radians
+        lat1 = (lat1 * 2.0 * Math.PI) / 60.0 / 360.0;      
+        long1 = (long1 * 2.0 * Math.PI) / 60.0 / 360.0;    
+        lat2 = (lat2 * 2.0 * Math.PI) / 60.0 / 360.0;   
+        long2 = (long2 * 2.0 * Math.PI) / 60.0 / 360.0;       
+  
+  
+        // use to different earth axis length    
+        var a = 6378137.0;        // Earth Major Axis (WGS84)    
+        var b = 6356752.3142;     // Minor Axis    
+        var f = (a-b) / a;        // "Flattening"    
+        var e = 2.0*f - f*f;      // "Eccentricity"      
+  
+        var beta = (a / Math.sqrt( 1.0 - e * Math.sin( lat1 ) * Math.sin( lat1 )));    
+        var cos = Math.cos( lat1 );    
+        var x = beta * cos * Math.cos( long1 );    
+        var y = beta * cos * Math.sin( long1 );    
+        var z = beta * ( 1 - e ) * Math.sin( lat1 );      
+  
+        beta = ( a / Math.sqrt( 1.0 -  e * Math.sin( lat2 ) * Math.sin( lat2 )));    
+        cos = Math.cos( lat2 );   
+        x -= (beta * cos * Math.cos( long2 ));    
+        y -= (beta * cos * Math.sin( long2 ));    
+        z -= (beta * (1 - e) * Math.sin( lat2 ));       
+  
+        return (Math.sqrt( (x*x) + (y*y) + (z*z) )/1000);  
       }
 
 
