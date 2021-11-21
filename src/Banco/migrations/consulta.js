550326f8b7ab6a6a3db6da344ddadaf5fab19484
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-21 07:41:38 -0300

    teste

--- src/Banco/migrations/consulta.js
+++ src/Banco/migrations/consulta.js
@@ -31,15 +31,15 @@ async function vendedor(token) {
   const user     = await verificaUser(token)
   
   if(user.status== false){
-    const erro = Funcoes.padraoErro("não foi possivel identificar o usuario da requisição")
+    const erro = Funcoes.padraoErro(`não foi possivel identificar o usuario da requisição`)
     return erro
   }else{
     
-    const vendedor    = await Banco.session('select v.id_vendedor, u.nome, p.cpf, t.telefone, e.cidade, t.whatsapp, e.rua, e.bairro, e.cidade, e.numero, e.cep, v.classificacao from Vendi.user u left outer join Vendi.pessoa   p on p.id_user=   u.id_user left outer join Vendi.telefone t on t.id_pessoa= p.id_pessoa left outer join Vendi.endereco e on e.id_pessoa= p.id_pessoa left outer join Vendi.vendedor v on v.id_pessoa= p.id_pessoa where u.id_user = $1 ',[user])
+    const vendedor    = await Banco.session(`select v.id_vendedor, u.nome, p.cpf, t.telefone, e.cidade, t.whatsapp, e.rua, e.bairro, e.cidade, e.numero, e.cep, v.classificacao from Vendi.user u left outer join Vendi.pessoa   p on p.id_user=   u.id_user left outer join Vendi.telefone t on t.id_pessoa= p.id_pessoa left outer join Vendi.endereco e on e.id_pessoa= p.id_pessoa left outer join Vendi.vendedor v on v.id_pessoa= p.id_pessoa where u.id_user = ${user} `)
     if(vendedor.rows[0]){
       return vendedor.rows[0]
     }
-    const erro = Funcoes.padraoErro("não foi encontrado resultados na base de dados")
+    const erro = Funcoes.padraoErro(`não foi encontrado resultados na base de dados`)
     return erro
   }
 }
