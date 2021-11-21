b6e211ce8476fb4f9889c61c8b7ff74be0104a18
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-21 13:15:59 -0300

    teste

--- src/controller/NegociacaoController.js
+++ src/controller/NegociacaoController.js
@@ -11,7 +11,7 @@ router.get('/whatsapp/:id', async (req, res) => {
     var text = req.body.mensagem
     
     var whatsapp = await Consulta.anuncioNegocicao(id)
-    console.log("---------------------------------"+whatsapp)
+    console.log("---------------------------------"+{whatsapp})
     text= text.replace(" ", "%20")
     res.json(`https://wa.me/55${whatsapp.telefone}?text=${text}`)
     //res.redirect(`https://wa.me/55${whatsapp.telefone}?text=${text}`)
